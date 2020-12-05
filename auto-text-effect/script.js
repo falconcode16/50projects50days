7d7776d903bbf6c386c7faa551e1c9df2921015e
Rohit <rohitpatil1625@gmail.com>
2020-12-05 10:29:21 +0530

    sdkh

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -1,8 +1,8 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text = 'We Love Programmers!'
 let idx = 1
-let speed = 300 / speedEl.value
+let speed = 302 / speedEl.value
 
 writeText()
 
